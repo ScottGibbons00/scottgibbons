10933fef9894a8283f942fb858c7c32b2dbb22e7
Scott Gibbons <William.s.gibbons.23@dartmouth.edu>
2020-05-17 00:25:19 -0400

    third commmit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Scott Gibbons
+          Scott Gibbons1
         </a>
       </header>
     </div>
